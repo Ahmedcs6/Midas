1f301e5fbe57868145cccf3e9db21c105d449448
Ahmed Mahmoud <ahmedmahmoudmos@gmail.com>
2026-09-08 04:27:00 +0300

    dotnet format

--- Midas.Api/Configuration/PostEntityTypeConfiguration.cs
+++ Midas.Api/Configuration/PostEntityTypeConfiguration.cs
@@ -3,19 +3,19 @@ namespace Midas.Api.Configuration;
 
 public class PostEntityTypeConfiguration : IEntityTypeConfiguration<Post>
 {
-    public void Configure(EntityTypeBuilder<Post> builder)
-    {
-        builder.ToTable("Post");
-        builder.Property(p => p.Content)
-                       .IsRequired()
-                       .HasMaxLength(5000);
+	public void Configure(EntityTypeBuilder<Post> builder)
+	{
+		builder.ToTable("Post");
+		builder.Property(p => p.Content)
+					   .IsRequired()
+					   .HasMaxLength(5000);
 
-        builder.Property(p => p.PublishDate)
-               .HasDefaultValueSql("GETUTCDATE()");
+		builder.Property(p => p.PublishDate)
+			   .HasDefaultValueSql("GETUTCDATE()");
 
-        builder.HasOne(p => p.User)
-               .WithMany(u => u.Posts)
-               .HasForeignKey(p => p.UserId)
-               .OnDelete(DeleteBehavior.Cascade);
-    }
+		builder.HasOne(p => p.User)
+			   .WithMany(u => u.Posts)
+			   .HasForeignKey(p => p.UserId)
+			   .OnDelete(DeleteBehavior.Cascade);
+	}
 }
